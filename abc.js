7e72130aa94a0133088edc4c52dac2c40180fb7d
Brian <brianzayas21@gmail.com>
2024-02-27 14:36:54 -0500

    pls

---
+++ abc.js
@@ -0,0 +1,14 @@
+//Declaring
+var userChoice, randomNumber, computerChoice
+userChoice = prompt("Choose heads or tails");
+randomNumber = Math.floor(Math.random()*2);
+if (randomNumber === 0) {
+    computerChoice = "heads";
+} else {
+    computerChoice = "tails";
+}
+if (userChoice.toLowerCase() === computerChoice) {
+    alert("You guessed right! The coin flip landed on " + userChoice);
+  } else {
+    alert("Sorry, the coin flip landed on " + userChoice);
+  }
