6e90f56d448da3c0fa1d1d81841dd44fa794a9ad
Brian <brianzayas21@gmail.com>
2024-02-27 14:53:21 -0500

    now

--- abc.js
+++ abc.js
@@ -1,4 +1,4 @@
-//Declaring
+//part 1
 var userChoice, randomNumber, computerChoice
 userChoice = prompt("Choose heads or tails");
 randomNumber = Math.floor(Math.random()*2);
@@ -12,3 +12,16 @@ if (userChoice.toLowerCase() === computerChoice) {
   } else {
     alert("Sorry, the coin flip landed on " + computerChoice);
   }
+//part 2
+var birthYear;
+birthYear = prompt("What year were you born?");
+var birthYearNumber = parseInt(birthYear);
+var currentYear = new Date().getFullYear();
+var age = currentYear - birthYearNumber
+if (age > 21) {
+    alert("You are " + age + " years old. You are old enough to drink in the US!");
+} else if (age === 21) {
+    alert("You are " + age + " years old. You are old enough to drink in the US...barely -_-");
+} else {
+    alert("You are " + age + " years old. Sorry, you are not old enough to drink amigo")
+}
